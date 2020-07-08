fad79765b740b9e2b790aee5e61e5573168c04b2
Thandanani Ngcobo <mxolic4@gmail.com>
2020-07-08 15:04:03 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8"> 
+	
+		
+        <title></title>
+        
+	<title>Naki Technologies</title>
+</head>
+<body>
+	
+	<!-- Heading section -->
+
+	<header>
+		<nav>
+		    
+			<div class="row" id="home"> <!-- Keep within 1140px width -->
+				<img src="resources/img/logo-white.png" alt="Naki Logo" class="logo">
+				<img src="resources/img/logo.png" alt="Naki Logo" class="logo-black">
+				<ul class="main-nav js--main-nav">  
+					<li><a href="#about">About US</a></li>
+					
+					<li><a href="#contact">Contact Us</a></li>
+				</ul>
+				<a class="mobile-nav-icon js--nav-icon"><i class="ion-navicon-round"></i></a> <!-- No href -->
+			</div>
+		</nav>
+		<div class="hero-text">
+		<h1>let us use your Data to tell a story<br>and improve your bottom line.</h1>
+			<a class="btn btn-full" href="#contact">Request a quotation</a>
+			<a class="btn btn-ghost" href="#contact">Acquire our services</a>
+		</div>
+	</header>
+	
+	<!-- Features section -->
+	
+	<section class="features js--features" id="about"> <!-- Use for sticky nav activation --> 
+		<div class="row">
+			<h2>who we are &mdash; and what we offer</h2> <!-- Longer dash + only use one h1, so using h2 -->
+			<p class="long-copy">
+				We’re a group of Data Scientists ,Software Developers and Statisticians, who came together after winning and performing well in hackathons and decided to start a company.
+			</p>
+		</div>
+		<!-- Grid using col -->
+		<div class="row js--wp-1">
+			<div class="col span-1-of-4 box">
+				<!-- Adding icons -->
+				<i class="icon ion-ios-analytics icon-big"></i>
+				<h3>Data Science</h3></h3>
+				<p> We use inter-disciplinary field that uses scientific methods, processes, algorithms and systems to extract knowledge and insights from structured and unstructured data.</p>
+			</div>
+			<div class="col span-1-of-4 box">
+			    
+				<i class="icon ion-ios-settings icon-big"></i>
+				<h3>Business Intelligence</h3>
+				<p> We leverage cloud software and services to transform data into actionable insights that inform an organization’s strategic and tactical business decisions.</p>
+			</div>
+			<div class="col span-1-of-4 box">
+			    <i class="icon ion-ios-book-outline icon-big"></i>
+				
+				<h3>EDU TECH</h3>
+				<p>We build technological solutions for the education and training industry, migrating them from the traditional ways of teaching into the forth industrial revolution.</p>
+			</div>
+			<div class="col span-1-of-4 box">
+
+				<i class="icon ion-ios-cog icon-big"></i>
+				<h3>Software</h3>
+				<p>We build software solutions that are a collection of instructions that enable the user to interact with a computer, its hardware, or perform tasks in websites, apps and chatbots.</p>
+			</div>
+		</div>
+	</section>
+	
+	<!-- Team section -->
+
+	
+	<!-- Form section -->
+
+	
+
+		
+		<div class="bootstrap-iso">
+		
+		 <div id="contact">
+            <div class="container">
+                <div class="row">
+                    <br><br>
+                    <div class="col-md-7">
+                        <div id="contact-left">
+                            
+                            <br>
+                            <h3>NAKI TECHNOLOGIES</h3>
+                            <br>
+                            <br>
+                            <p>
+                                We believe in <strong>Simple</strong>, 
+                                <strong>Clean</strong> & Modern Design 
+                                standards with a responsive Approach.
+                                Let us use your Data to tell a story<br>and improve your bottom line.
+                            </p>
+                            <div id="contact-info">
+                                <address>
+                                    <strong>Headquarters</strong>
+                                    <br>
+                                    <p>Gauteng<br>
+                                        Johanesburg 2001
+                                    </p>
+                                        
+     
+                                </address>
+                                <div id="phone-fax-email">
+                                    <p>
+                                        <strong>Phone:
+                                        <span>0815908833</span><br>
+                                        </strong>
+                                        
+                                        
+                                        <strong>Email:
+                                        <span>info@nakitechnologies.co.za</span><br>
+                                        </strong>
+                
+                                    </p>
+                                
+                                </div>
+               
+                            </div>
+                            
+                            <ul class="social-list">
+                            <li><a href="" class="social-icon"><i class="fa fa-facebook"></i></a></li>
+                                
+                            <li><a href="" class="social-icon"><i class="fa fa-twitter"></i></a></li>
+                                
+                                <li><a href="" class="social-icon"><i class="fa fa-youtube-play"></i></a></li>
+                                
+                            <li><a href="" class="social-icon"><i class="fa fa-google-plus"></i></a></li>
+                          
+                            </ul>
+             
+                        </div>
+                    </div>
+                    
+                    <div class="col-md-5">
+                        <div id="contact-right">
+                            
+                            <br>
+                            <h3> Contact Us</h3>
+                            
+                            <form method="post" action="">
+                                <input type="text" name="name" placeholder="Full Name" class="form-control" required>
+                                
+                                <input type="text" name="email" placeholder="Email Address" class="form-control" required>
+                                
+                                <textarea name="message" placeholder="message.." rows="5" class="form-control" required></textarea>
+                                <div class="g-recaptcha" data-sitekey="6LfBid0UAAAAAHkji7GerOolRGXJ-EFiUK1NeZP4"></div>
+                                <input type="submit" name="submit" value="Send Message" class="submit-btn">
+                                    
+                            </form>
+							<div class="status">
+							
+								</div>
+                        </div>
+                    
+                </div>
+            </div>
+        </div>
+           </div> 
+        <br>
+        <br>
+        <div id="footer-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div id="footer-copyrights">
+                            <p>Copyrights &copy; 2020 NAKI Tech. All rights reserved</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div id="footer-menu">
+                            <ul>
+                                <li><a href="#">Home</a></li>
+                                
+                                <li><a href="#about">About Us</a></li>
+                                
+                            </ul>
+                        </div>   
+                    </div>
+                </div>
+            </div>
+     </div>
+    
+
+	<script src="//cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
+	<script src="//cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="//cdn.jsdelivr.net/selectivizr/1.0.3b/selectivizr.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="vendors/js/jquery.waypoints.min.js"></script>
+	<script src="resources/js/scripts.js"></script> <!-- Sticky nav -->
+	
+	
+</body>
+</html>
